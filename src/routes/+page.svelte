5a64a8cddfc114a4e75df5df384f50aab78ff87b
free5all <bmbkr4414@gmail.com>
2025-08-04 20:56:26 +0000

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation mwhahahahahahaha</p>
+<div class="flex flex-col items-center justify-center w-full h-screen select-none">
+    <h1 class="text-3xl font-bold mb-4 text-shadow-sm text-zinc-500">Stay tuned! Something big is coming soon.</h1>
+    <img src="https://png.pngtree.com/png-clipart/20231107/original/pngtree-stag-beetle-deer-picture-image_13239881.png" alt="Stag beetle" width="200"/>
+</div>
